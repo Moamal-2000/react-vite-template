246fc1b290cd792fb6bfd35ed03ba2956dbef06a
Moamal-2000 <moamalalaapro1@gmail.com>
2024-12-13 12:15:36 +0300

    deprecate: replace @import with @use in scss files

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,6 +1,10 @@
 import s from "./Home.module.scss";
 
 const Home = () => {
-  return <main className={s.home}>Home Page</main>;
+  return (
+    <main className={s.home}>
+      <h1>React + Vite Project template</h1>
+    </main>
+  );
 };
 export default Home;
