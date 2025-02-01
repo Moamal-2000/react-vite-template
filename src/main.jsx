ce39c83189427dcb34bf5c2eaf277205fcbd0853
Moamal-2000 <moamalalaapro1@gmail.com>
2025-02-01 20:41:42 +0300

    fix: correct import path for GlobalContext component

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
 import { store } from "./App/store.js";
-import GlobalContextProvider from "./Context/globalContext.jsx";
+import GlobalContextProvider from "./Context/GlobalContext.jsx";
 import "./Styles/index.scss";
 
 createRoot(document.getElementById("root")).render(
