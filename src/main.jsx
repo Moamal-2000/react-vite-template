65ee0199e08d808dba9dbfd3630e083f07881e34
Moamal-2000 <moamalalaapro1@gmail.com>
2025-08-27 12:35:30 +0300

    remove(context): remove the global context

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,12 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
 import { store } from "./App/store.js";
-import GlobalContextProvider from "./Context/GlobalContext.jsx";
 import "./Styles/index.scss";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GlobalContextProvider>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </GlobalContextProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
